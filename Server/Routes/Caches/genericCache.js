6a36d3440efd4770faf2fae000616299fff51616
CharlieSaunders <54174245+CharlieSaunders@users.noreply.github.com>
2022-09-29 19:03:43 +0100

    Added cachelogger
    Added a static class to log to console with nice colours. Doesn't add anything major just makes the console easier to read and more apparent as to what update happened

--- Server/Routes/Caches/genericCache.js
+++ Server/Routes/Caches/genericCache.js
@@ -11,30 +11,52 @@ class GenericCache {
         cacheObjects.forEach(element => {
             this.objects[element._id] = element;
         });
-        console.log(`SET* ==> Setting cache objects for ${this.cacheName}`)
+        CacheLogger.LogSetup(this.cacheName);
     }
 
     setSingle(object){
-        console.log(`SET ==> ${this.cacheName} set for: ID --> ${object._id}`);
+        CacheLogger.LogSet(this.cacheName, object._id);
         this.objects[object._id] = object;
     }
 
     get(){
-        console.log(`GET* ==> Getting ${this.cacheName} objects`)
+        CacheLogger.LogGet(this.cacheName, null);
         return Object.values(this.objects);
     }
 
     getSingle(id){
-        console.log(`GET ==> Getting ${this.cacheName} object: ID --> ${id}`)
+        CacheLogger.LogGet(this.cacheName, id);
         return this.objects[id];
     }
 
     unset(id){
-        console.log(`DELETE ==> ${this.cacheName} delete for: ID --> ${id}`);
+        CacheLogger.LogDelete(this.cacheName, id);
         let values = this.getObjects();
         let index = values.map(x => x._id.toString()).indexOf(id);
         values.splice(index, 1);
         this.set(values);
+    }
+}
+
+class CacheLogger {
+    // Set foreground colour, then use string interpolation '%s' then set the text colour back to white
+    static LogSetup(cacheName){
+        console.log('\x1b[32m%s\x1b[0m', `==> Setup cache objects for ${cacheName}`);
+    }
+
+    static LogSet(cacheName, id){
+        console.log('\x1b[34m%s\x1b[0m', `==> Getting ${cacheName} object: ID --> ${id}`);
+    }
+
+    static LogGet(cacheName, id){
+        if(id == null)
+            console.log('\x1b[35m%s\x1b[0m', `==> Getting ${cacheName} objects`);
+        else
+            console.log('\x1b[35m%s\x1b[0m', `==> Getting ${cacheName} object. ID --> ${id}`);
+    }
+
+    static LogDelete(cacheName){
+        console.log(`\x1b[31m%s\x1b[0m`, `==> Deleting ${cacheName} object. ID --> ${id}`);
     }
 }
 
